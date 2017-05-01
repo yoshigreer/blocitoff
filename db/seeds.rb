6643f97d6eda1fe3da5ae8aa5b6ba3db1ab3a4d7
Trenton Greer <yoshigreer@gmail.com>
2017-04-30 22:42:04 -0600

    seeding complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+include Faker
+
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet::password
+  )
+end
+
+users = User.all
+
+100.times do
+  Item.create!(
+    name: Faker::Lorem.sentence,
+    user: users.sample
+  )
+end
+
+items = Item.all
+
+member = User.create!(
+  email: 'member@example.com',
+  password: 'helloworld'
+)
+
+puts "seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} wikis created"
